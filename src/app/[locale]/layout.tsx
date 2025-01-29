d87467824857e52245d6b111081617944ecd9e67
Dariusz Niemczyk <palid@phoelid.me>
2025-01-29 18:57:02 +0100

    fix build issue

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -13,7 +13,7 @@ export default async function RootLayout({
   params
 }: {
   children: React.ReactNode
-  params: { locale: Lang }
+  params: Promise<{ locale: Lang }>
 }) {
   const { locale } = await params
   const currentLang = locales.includes(locale) ? locale : "en"
