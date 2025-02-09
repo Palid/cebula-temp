6789cdd9628b0574b22acb70b48b542b1e58f258
Serge Bazanski <q3k@q3k.org>
2025-02-09 22:48:44 +0100

    leaflet: place pin more accurately

--- src/components/event-map.lazy.tsx
+++ src/components/event-map.lazy.tsx
@@ -13,12 +13,12 @@ export default function Map({ t }: {
   return (
     <div className='h-screen max-h-[400px] pt-8 relative mx-auto max-w-3xl'>
       {/* @ts-expect-error dragging IS a valid prop. */}
-      <MapContainer dragging={false} center={[51.105173, 17.087157]} zoom={40} scrollWheelZoom={false} className="h-full w-full">
+      <MapContainer dragging={false} center={[51.104955057760804, 17.087378768775697]} zoom={40} scrollWheelZoom={false} className="h-full w-full">
         {/* @ts-expect-error attribution IS a valid prop. */}
         <TileLayer attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        <Marker position={[51.105173, 17.087157]}>
+        <Marker position={[51.104955057760804, 17.087378768775697]}>
           <Popup>
             {t.where.location}
           </Popup>
