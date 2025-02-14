e11dcdf071ece004b00035545ed213dfb73789c8
Dariusz Niemczyk <palid@phoelid.me>
2025-02-15 00:33:10 +0100

    feat: add better loading and 404 page

--- src/app/[locale]/loading.tsx
+++ src/app/[locale]/loading.tsx
@@ -5,7 +5,7 @@ import Image from "next/image"
 export default function Loading() {
   return (
     <div className="fixed inset-0 flex items-center justify-center bg-background/80 backdrop-blur-sm">
-      <div className="relative h-16 w-16 animate-spin">
+      <div className="relative h-full w-full max-w-[50vw] max-h-[50vh] motion-reduce:animate-bounce motion-safe:animate-spin">
         <Image src="/web-app-manifest-512x512.png" alt="Loading..." fill className="object-contain" priority />
       </div>
     </div>
--- src/app/[locale]/not-found.tsx
+++ src/app/[locale]/not-found.tsx
@@ -1,5 +1,21 @@
+"use client"
 
+import { jgs7 } from "@/fonts"
+import Image from "next/image"
 
 export default function NotFound() {
-  return <div>Not Found</div>
+  return (
+    <div>
+      <div className="fixed inset-0 flex items-center justify-center bg-background/80 backdrop-blur-sm flex-col">
+        <section className={`text-6xl flex flex-col text-center`}>
+          <span className={`${jgs7.className} text-8xl`}>404</span>
+          <span>Page not found</span>
+        </section>
+        <div className="fixed h-full w-full max-w-[50vw] max-h-[50vh] motion-safe:animate-ping ">
+          <Image src="/web-app-manifest-512x512.png" alt="Loading..." fill className="object-contain" priority />
+        </div>
+      </div>
+    </div>
+  )
 }
+
